6d836d9c3728f568b9911cbf38af0a570f4251f1
Esdras Abdir <esdrasabdirissacar2@gmail.com>
2025-08-24 21:45:38 -0300

    Exemplo de método para a classe str.

---
+++ aula33.py
@@ -0,0 +1,7 @@
+""" 
+Imutáveis que vimos : str, int, float, bool
+"""
+string = 'Esdras Abdir'
+# outra_variavel = f'{string[:3]}bc'
+
+print(string.zfill(100)) # exemplo de método para objeto string
