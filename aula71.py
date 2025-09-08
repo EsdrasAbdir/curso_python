1e704a97c199c10425644aacd8a29ad748d469ca
Esdras Abdir <esdrasabdirissacar2@gmail.com>
2025-09-08 15:45:57 -0300

    Subindo a aula 71

---
+++ aula71.py
@@ -0,0 +1,26 @@
+""" 
+Retorno de valores das funções (return)
+"""
+
+def soma(x,y):
+    if x > 10:
+        return 10
+    return x + y
+
+
+
+
+
+soma1 = soma(11,2)
+soma2 = soma(3,3)
+
+print(soma1 + soma2)
+
+# variavel = soma(1,2)
+# variavel = int('1')
+
+
+
+# variavel = print('Esdras')
+#  variavel = None
+# print(variavel is None)
