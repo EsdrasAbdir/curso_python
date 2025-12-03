f5be746a72041fcb0e03779de8f7024d75fea377
Esdras Abdir <esdrasabdirissacar2@gmail.com>
2025-12-02 22:10:17 -0300

    Subindo aula 96.py

---
+++ aula96.py
@@ -0,0 +1,18 @@
+# for dentro de for em uma lista comprehension
+
+
+# lista = []
+
+# for x in range(3):
+#     for y in range(3):
+#         lista.append((x,y))
+
+
+lista = [
+    (x,y) 
+    for x in range(3)
+    for y in range(3)
+
+]
+
+print(lista)
