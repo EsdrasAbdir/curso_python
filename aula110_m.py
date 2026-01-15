6fb055324ff93b13c625fcb205524c27592484f2
Esdras Abdir <esdrasabdirissacar2@gmail.com>
2026-01-15 19:30:48 -0300

    Subindo aula 111.py e modificação da aula 110_m.py

--- aula110_m.py
+++ aula110_m.py
@@ -1,1 +1,4 @@
 print('Este módulo se chama',__name__)
+
+
+variavel_modulo = 'Esdras'
---
+++ aula111.py
@@ -0,0 +1,19 @@
+""" 
+Modularização - Entendendo os seus próprios módulos Python
+O primeiro módulo executado chama-se __main__
+Você pode importar outro módulo inteiro ou parte do módulo
+
+O python conhece a pasta onde o __main__ está e as pastas
+abaixo dele
+Ele não reconhece pastas e módulos acima do __main__ por 
+padrão
+O python conhece todos os módulos e pacotes presentes 
+nos caminhos sys.path
+"""
+
+import aula110_m
+
+from aula110_m import variavel_modulo
+# print('Este módulo se chama',__name__)
+
+print(variavel_modulo)
