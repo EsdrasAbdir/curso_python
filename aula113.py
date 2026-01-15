b80deb66ad754c41bac184bef82e3a9cd23f731e
Esdras Abdir <esdrasabdirissacar2@gmail.com>
2026-01-15 20:17:29 -0300

    Subindo aula113.py e aula113_package com dois módulos py

---
+++ aula113.py
@@ -0,0 +1,21 @@
+# from sys import path
+
+# import aula113_package.modulo
+# from aula113_package import modulo
+# from aula113_package.modulo import *
+
+#  from aula113_package.modulo import soma_do_modulo
+
+#  print(*path, sep='\n')
+
+# print(soma_do_modulo(1,2))
+
+# print(aula113_package.modulo.soma_do_modulo(1,2))
+
+# print(modulo.soma_do_modulo(1,2))
+
+# print(variavel)
+
+from aula113_package.modulo import fala_oi,soma_do_modulo
+print(__name__)
+fala_oi()
---
+++ aula113_package/modulo.py
@@ -0,0 +1,13 @@
+# __all__ = [
+#     'variavel', 'soma_do_modulo'
+# ]
+
+from aula113_package.modulo_b import fala_oi
+
+variavel = 'alguma coisa'
+
+def soma_do_modulo(x, y):
+    return x + y
+
+fala_oi()
+
